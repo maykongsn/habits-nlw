c278b4be9083e8d801ddcf4ed67468f4c3da2657
maykongsn <contatomaykon@hotmail.com>
2023-01-20 21:43:02 -0300

    feat: adicionando os dias na tabela

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,4 +1,12 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+import { HabitDay } from "./HabitDay";
+
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDatesSize = 18 * 7;
+const amountOfSummaryDatesToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
     return (
@@ -13,18 +21,19 @@ export function SummaryTable() {
                 })}
             </div>
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                {summaryDates.map((date) => {
+                    return <HabitDay key={date.toString()}/>
+                })}
 
+                {amountOfSummaryDatesToFill > 0 && Array.from({ length: amountOfSummaryDatesToFill }).map((_, i) => {
+                    return (
+                        <div
+                            key={i} 
+                            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                        />
 
+                    )
+                })}
             </div>
         </div>
     );
