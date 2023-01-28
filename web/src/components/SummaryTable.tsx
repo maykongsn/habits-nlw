55180766d4703926a6076fd2d72e61234f1492d2
maykongsn <contatomaykon@hotmail.com>
2023-01-27 22:58:58 -0300

    feat: adicionando informações da api na tabela

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
 
@@ -8,7 +11,21 @@ const summaryDates = generateDatesFromYearBeginning();
 const minimumSummaryDatesSize = 18 * 7;
 const amountOfSummaryDatesToFill = minimumSummaryDatesSize - summaryDates.length;
 
+type Summary = {
+    id: string;
+    date: string;
+    amount: number;
+    completed: number;
+}[]
+
 export function SummaryTable() {
+    const [summary, setSummary] = useState<Summary>([]);
+
+    useEffect(() => {
+        api.get("/summary").then(response => {
+            setSummary(response.data);
+        })
+    }, [])
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -22,7 +39,17 @@ export function SummaryTable() {
             </div>
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map((date) => {
-                    return <HabitDay amount={5} completed={Math.round(Math.random() * 5)} key={date.toString()}/>
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(date).isSame(day.date, 'day')
+                    });
+                    return (
+                        <HabitDay 
+                            key={date.toString()}
+                            date={date}
+                            amount={dayInSummary?.amount} 
+                            completed={dayInSummary?.completed} 
+                        />
+                    )
                 })}
 
                 {amountOfSummaryDatesToFill > 0 && Array.from({ length: amountOfSummaryDatesToFill }).map((_, i) => {
